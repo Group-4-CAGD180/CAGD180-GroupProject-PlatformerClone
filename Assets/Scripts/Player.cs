d1f454da490ab20db31d58721761a17326c9ba71
Wade Lawler <lawlerwade@gmail.com>
2025-04-03 17:40:18 -0700

    Created player script and attached to player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+/*
+ * Author: Wade Lawler
+ * Date Created: 4/3/25
+ * Last Modified: 4/3/25
+ * Description: This script handles player functionality, such as input,
+ *              movement, respawning, etc.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
